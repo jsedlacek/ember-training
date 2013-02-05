0b0b66c78ca2f7c2dbc14f4ca2477c627efdfbb0
tomhuda <tomhuda@tilde.io>
2013-02-05 11:46:31 -0800

    Use models, fixtures and Handlebars helpers

--- js/app.js
+++ js/app.js
@@ -5,5 +5,94 @@
 window.App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.resource("album", { path: "/album" });
+  this.resource("album", { path: "/album/:album_id" });
 });
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return App.Album.find();
+  }
+});
+
+Ember.Handlebars.registerBoundHelper('minutes', function(seconds) {
+  var formattedMinutes = Math.floor(seconds / 60);
+  var formattedSeconds = (seconds - (formattedMinutes * 60));
+  return formattedMinutes + ":" + formattedSeconds;
+});
+
+var attr = DS.attr, hasMany = DS.hasMany, belongsTo = DS.belongsTo;
+
+App.Store = DS.Store.extend({
+  revision: 11,
+  adapter: 'DS.FixtureAdapter'
+});
+
+App.Album = DS.Model.extend({
+  artwork: attr('string'),
+  name: attr('string'),
+  artist: attr('string'),
+  songs: hasMany('App.Song'),
+  isExplicit: attr('boolean')
+});
+
+App.Song = DS.Model.extend({
+  track: attr('number'),
+  name: attr('string'),
+  duration: attr('number'),
+
+  album: belongsTo('App.Album')
+});
+
+App.Album.FIXTURES = [{
+  id: 1,
+  artwork: "http://cdn3.rd.io/album/d/b/c/0000000000220cbd/1/square-200.jpg",
+  name: "The Morning After",
+  artist: "GOLDHOUSE",
+  songs: [ 11, 12, 13, 14 ]
+}, {
+  id: 2,
+  artwork: "http://cdn3.rd.io/album/0/1/3/0000000000279310/1/square-200.jpg",
+  name: "Dusk to Dawn",
+  artist: "Emancipator",
+  songs: [ 21, 22, 23 ]
+}, {
+  id: 3,
+  artwork: "http://cdn3.rd.io/album/d/d/5/00000000001e25dd/3/square-200.jpg",
+  name: "The Heist",
+  artist: "Macklemore & Ryan Lewis",
+  isExplicit: true,
+  songs: [ 31, 32, 33 ]
+}, {
+  id: 4,
+  artwork: "http://cdn3.rd.io/album/f/0/b/0000000000152b0f/8/square-200.jpg",
+  name: "Some Nights",
+  artist: "fun.",
+  isExplicit: true,
+  songs: [ 41, 42, 43 ]
+}];
+
+App.Song.FIXTURES = [{
+  id: 11,
+  track: 1,
+  name: "A Walk",
+  duration: 316,
+  album: 1
+}, {
+  id: 12,
+  track: 2,
+  name: "Hours",
+  duration: 344,
+  album: 1
+}, {
+  id: 13,
+  track: 3,
+  name: "Daydream",
+  duration: 334,
+  album: 1
+}, {
+  id: 14,
+  track: 4,
+  name: "Dive",
+  duration: 499,
+  album: 1
+}];

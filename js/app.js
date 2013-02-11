ce6313155503aa90d1cfdab32405f6a9bdf32c05
tomhuda <tomhuda@tilde.io>
2013-02-10 22:46:35 -0800

    Step 2

--- js/app.js
+++ js/app.js
@@ -3,8 +3,19 @@
 
 window.App = Ember.Application.create();
 
+App.Store = DS.Store.extend({
+  revision: 11,
+  adapter: 'DS.FixtureAdapter'
+});
+
 App.Router.map(function() {
   this.route('album');
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return App.Album.find();
+  }
 });
 
 })();

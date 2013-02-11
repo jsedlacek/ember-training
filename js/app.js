4fc2989064582efda413330a7183d8f1a6bf9837
Standa Opichal <opichals@gmail.com>
2013-02-11 20:56:12 +0100

    Static Index and Album 'mockups'.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 (function() {
 "use strict";
 
+window.App = Em.Application.create();
+
+//App.store = DS.Store.create({
+//    revision: 12,
+//    adapter: 'DS.FixtureAdapter'
+//});
+
+App.Router.map(function() {
+    this.route('album');
+});
+
 })();

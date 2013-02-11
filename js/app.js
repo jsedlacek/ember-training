c0a2ce93f202c3e80b2731203b4bf23762b53e2a
Standa Opichal <opichals@gmail.com>
2013-02-11 20:56:12 +0100

    Morning.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 (function() {
 "use strict";
 
+window.App = Em.Application.create();
+
+//App.store = DS.Store.create({
+//    revision: 11,
+//    adapter: 'DS.FixtureAdapter'
+//});
+//
+App.Router.map(function() {
+    this.route('album');
+});
+
 })();
